208dea77f6b9004fded68d2a6065e43440fb3129
unknown <alexmont4793@gmail.com>
2019-05-31 17:04:17 -0500

    Updates

---
+++ precache-manifest.62af92d7aeaef9d49cc5a526828f102c.js
@@ -0,0 +1,114 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9649b3aa18a6716665f9a5d2f054126a",
+    "url": "/Montealegre_Sti/index.html"
+  },
+  {
+    "revision": "2397be7df6c75fab84ae",
+    "url": "/Montealegre_Sti/static/css/2.d81b03a1.chunk.css"
+  },
+  {
+    "revision": "b9ca95b0a6b3ede3aff8",
+    "url": "/Montealegre_Sti/static/css/main.fb096fc7.chunk.css"
+  },
+  {
+    "revision": "2397be7df6c75fab84ae",
+    "url": "/Montealegre_Sti/static/js/2.805d2706.chunk.js"
+  },
+  {
+    "revision": "b9ca95b0a6b3ede3aff8",
+    "url": "/Montealegre_Sti/static/js/main.cbddb96b.chunk.js"
+  },
+  {
+    "revision": "bb7fbe06678839aa856a",
+    "url": "/Montealegre_Sti/static/js/runtime~main.13228d2a.js"
+  },
+  {
+    "revision": "9abb32453da969dc372807779f139e77",
+    "url": "/Montealegre_Sti/static/media/banner.9abb3245.png"
+  },
+  {
+    "revision": "13db00b7a34fee4d819ab7f9838cc428",
+    "url": "/Montealegre_Sti/static/media/brand-icons.13db00b7.eot"
+  },
+  {
+    "revision": "a046592bac8f2fd96e994733faf3858c",
+    "url": "/Montealegre_Sti/static/media/brand-icons.a046592b.woff"
+  },
+  {
+    "revision": "a1a749e89f578a49306ec2b055c073da",
+    "url": "/Montealegre_Sti/static/media/brand-icons.a1a749e8.svg"
+  },
+  {
+    "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
+    "url": "/Montealegre_Sti/static/media/brand-icons.c5ebe0b3.ttf"
+  },
+  {
+    "revision": "e8c322de9658cbeb8a774b6624167c2c",
+    "url": "/Montealegre_Sti/static/media/brand-icons.e8c322de.woff2"
+  },
+  {
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "/Montealegre_Sti/static/media/flags.9c74e172.png"
+  },
+  {
+    "revision": "0ab54153eeeca0ce03978cc463b257f7",
+    "url": "/Montealegre_Sti/static/media/icons.0ab54153.woff2"
+  },
+  {
+    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
+    "url": "/Montealegre_Sti/static/media/icons.8e3c7f55.eot"
+  },
+  {
+    "revision": "962a1bf31c081691065fe333d9fa8105",
+    "url": "/Montealegre_Sti/static/media/icons.962a1bf3.svg"
+  },
+  {
+    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
+    "url": "/Montealegre_Sti/static/media/icons.b87b9ba5.ttf"
+  },
+  {
+    "revision": "faff92145777a3cbaf8e7367b4807987",
+    "url": "/Montealegre_Sti/static/media/icons.faff9214.woff"
+  },
+  {
+    "revision": "8f8c2f92f26e545a9b1fac71eec99291",
+    "url": "/Montealegre_Sti/static/media/img.8f8c2f92.png"
+  },
+  {
+    "revision": "8261c6c084670489cc7b692440c363c0",
+    "url": "/Montealegre_Sti/static/media/logo.8261c6c0.png"
+  },
+  {
+    "revision": "325a7bb59f002bbd03dc3725727df779",
+    "url": "/Montealegre_Sti/static/media/lunch-box.325a7bb5.png"
+  },
+  {
+    "revision": "f549a3e6eebe281a38dfb9a5531a6401",
+    "url": "/Montealegre_Sti/static/media/microphone.f549a3e6.png"
+  },
+  {
+    "revision": "701ae6abd4719e9c2ada3535a497b341",
+    "url": "/Montealegre_Sti/static/media/outline-icons.701ae6ab.eot"
+  },
+  {
+    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
+    "url": "/Montealegre_Sti/static/media/outline-icons.82f60bd0.svg"
+  },
+  {
+    "revision": "ad97afd3337e8cda302d10ff5a4026b8",
+    "url": "/Montealegre_Sti/static/media/outline-icons.ad97afd3.ttf"
+  },
+  {
+    "revision": "cd6c777f1945164224dee082abaea03a",
+    "url": "/Montealegre_Sti/static/media/outline-icons.cd6c777f.woff2"
+  },
+  {
+    "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
+    "url": "/Montealegre_Sti/static/media/outline-icons.ef60a4f6.woff"
+  },
+  {
+    "revision": "37173d9b99cbbecc5328668873b77efb",
+    "url": "/Montealegre_Sti/static/media/wine.37173d9b.png"
+  }
+]);
